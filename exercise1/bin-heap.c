750ad562f50a19c96aa0309bd3eb35c136733f5e
Bufas <bufas7@gmail.com>
2013-09-09 20:40:17 +0200

    Change bubble_up to use rotate_up

--- exercise1/bin-heap.c
+++ exercise1/bin-heap.c
@@ -95,49 +95,7 @@ void decrease_key(heap *h, node *n, int delta) {
 
 void bubble_up(heap *h, node *n) {
     while (n->parent != NULL && n->parent->key > n->key) {
-        // Set variables
-        node *parent = n->parent;
-        node *left_child = n->left_child;
-        node *right_child = n->right_child;
-        node *grandparent = NULL;
-        if(parent != NULL) grandparent = n->parent->parent;
-
-        // Swap n with its parent, there are 10 pointers to update
-        if(left_child != NULL) left_child->parent = parent;   // 1
-        if(right_child != NULL) right_child->parent = parent; // 2
-
-        // Find out if n is the right or left child
-        node *sibling = NULL;
-        if (n == parent->left_child) {
-            // n is the left child
-            sibling = parent->right_child;
-            n->left_child = parent;                           // 4
-            n->right_child = sibling;                         // 5
-        } else {
-            // n is the right child
-            sibling = parent->left_child;
-            n->left_child = sibling;                          // 4
-            n->right_child = parent;                          // 5
-        }
-
-        if (sibling != NULL) sibling->parent = n;             // 3
-
-        // Find out if parent is right or left child
-        if(grandparent != NULL) { 
-            if (parent == grandparent->left_child) {
-                // parent is left child
-                grandparent->left_child = n;                  // 6
-            } else {
-                // parent is right child
-                grandparent->right_child = n;                 // 6
-            }
-        }
-
-        parent->left_child = left_child;                      // 7
-        parent->right_child = right_child;                    // 8
-
-        parent->parent = n;                                   // 9
-        n->parent = grandparent;                              // 10
+        rotate_up(n);
     }
 
     // If n got to the top, update the root
@@ -174,8 +132,8 @@ void delete_min(heap *h) {
     last->parent = NULL;                                    // 4
 
     // Update old root's children's parent pointers
-    root->left_child->parent = last;
-    root->right_child->parent = last;
+    root->left_child->parent = last;                        // 5
+    root->right_child->parent = last;                       // 6
 
     // Update the heap fields
     h->size -= 1;
@@ -208,7 +166,6 @@ void rotate_up(node *n) {
 
     // Find out if n is the right or left child
     node *sibling = NULL;
-    if (parent == NULL) printf("parent is null\n");
     if (n == parent->left_child) {
         // n is the left child
         sibling = parent->right_child;
@@ -288,6 +245,7 @@ int main() {
     for (int i = 10; i > 0; i--) {
         insert(i, h);
     }
+
     for (int i = 11; i <= 15; i++) {
         insert(i, h);
     }
