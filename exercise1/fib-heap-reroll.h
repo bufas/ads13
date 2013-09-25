86293d0442bda4aebc287c61d197e2187c117b6c
Bufas <bufas7@gmail.com>
2013-09-25 23:15:18 +0200

    This was missing

---
+++ exercise1/fib-heap-reroll.h
@@ -0,0 +1,28 @@
+#ifndef FIB_HEAP_H
+#define FIB_HEAP_H
+
+typedef struct node node;
+typedef struct heap heap;
+
+struct heap {
+    node *root;
+    int size;
+};
+
+struct node {
+    node* parent;           // Parent node
+    node* child;            // Any child node
+    node* left_sibling;     // Left sibling
+    node* right_sibling;    // Right sibling
+    int key;                // Priority
+    int rank;               // Rank >= 0
+    int marked;             // Unmarked = 0, marked = 1
+};
+
+heap* make_heap();
+node find_min(heap *h);
+node* insert(heap *h, int key);
+node* delete_min(heap *h);
+void decrease_key(heap *h, node *n, int delta);
+
+#endif
